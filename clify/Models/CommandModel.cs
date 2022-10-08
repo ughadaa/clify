4d29227aaf03263c0eb8a74294f7812c9236329f
ratina <ghadakj@hotmail.com>
2022-10-09 00:43:44 +0300

    added Commands Model

---
+++ clify/Models/CommandModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace clify.Models
+{
+    public class CommandModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string des { get; set; }
+        public CommandModel(int id, string name, string des)
+        {
+            this.id = id;
+            this.name = name;
+            this.des = des;
+
+        }
+    }
+}
+
